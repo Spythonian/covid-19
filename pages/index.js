cb089306146dd01622a6caa230b5a5a13ea27f3f
Idris Sulaiman Ademola <spythonian@gmail.com>
2020-05-11 00:27:05 +0100

    Implemented the api on the index page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,43 @@
+import React from 'react';
 import Head from 'next/head';
 import Cards from '../components/Cards/Cards';
 import Charts from '../components/Charts/Charts';
 import CountryPicker from '../components/CountryPicker/CountryPicker';
 
+import { fetchData } from '../api/index';
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div>
-          <h1>This is the home interface</h1>
-        </div>
-      </main>
-    </div>
-  );
+class Home extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+    // console.log(data);
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return [
+      <div>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <div>
+            <h1>This is the home interface</h1>
+          </div>
+          <Cards data={data} />
+          <CountryPicker />
+          <Charts />
+        </main>
+      </div>,
+    ];
+  }
 }
+
+export default Home;
