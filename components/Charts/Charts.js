1bde4ce7ff2288c837837efc291dd318df5162d2
Idris Sulaiman Ademola <spythonian@gmail.com>
2020-05-10 18:14:03 +0100

    Created and link required components

---
+++ components/Charts/Charts.js
@@ -0,0 +1,8 @@
+export default function Charts() {
+    return (
+      <div>
+        <h1>This is the Charts</h1>
+      </div>
+    );
+  }
+  
---
+++ components/CountryPicker/CountryPicker.js
@@ -0,0 +1,7 @@
+export default function CountryPicker() {
+  return (
+    <div>
+      <h1>This is the CountryPicker</h1>
+    </div>
+  );
+}
