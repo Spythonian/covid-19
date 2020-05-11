0734423668c4f5889df134b380e19372af0559c3
Idris Sulaiman Ademola <spythonian@gmail.com>
2020-05-11 16:03:59 +0100

    Fetched daily data from the api

--- components/Charts/Charts.js
+++ components/Charts/Charts.js
@@ -1,8 +1,44 @@
+import { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api/index';
+
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Charts.module.css';
+
 export default function Charts() {
-    return (
-      <div>
-        <h1>This is the Charts</h1>
-      </div>
-    );
-  }
-  
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    // console.log(dailyData);
+    fetchApi();
+  }, []);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: 'Deaths',
+            borderColor: '#red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
+}
