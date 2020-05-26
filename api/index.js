0c08d715c9c764ca5a5f10a3af19562ad24c3b0c
Idris Sulaiman Ademola <spythonian@gmail.com>
2020-05-26 19:59:56 +0100

    Worked with the country

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
 
     return {
       confirmed,
@@ -16,7 +22,9 @@ export const fetchData = async () => {
     };
 
     // console.log(response);
-  } catch (error) {}
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const fetchDailyData = async () => {
@@ -30,5 +38,19 @@ export const fetchDailyData = async () => {
     }));
     // console.log(data);
     return modifiedData;
-  } catch (error) {}
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (err) {
+    console.log(err);
+  }
 };
